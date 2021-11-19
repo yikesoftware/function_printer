a31ee799449fe4d03fdc243aa5f7030e091e054a
yikesoftware <1532494123@qq.com>
2021-11-19 22:37:32 +0800

    new

--- out/build/x64-Debug/parser/cmake_install.cmake
+++ out/build/x64-Debug/parser/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: F:/work/function_printer
+# Install script for directory: F:/work/function_printer/parser
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -32,13 +32,3 @@ if(NOT DEFINED CMAKE_CROSSCOMPILING)
   set(CMAKE_CROSSCOMPILING "FALSE")
 endif()
 
-if(CMAKE_INSTALL_COMPONENT)
-  set(CMAKE_INSTALL_MANIFEST "install_manifest_${CMAKE_INSTALL_COMPONENT}.txt")
-else()
-  set(CMAKE_INSTALL_MANIFEST "install_manifest.txt")
-endif()
-
-string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
-       "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "F:/work/function_printer/out/build/x64-Debug/${CMAKE_INSTALL_MANIFEST}"
-     "${CMAKE_INSTALL_MANIFEST_CONTENT}")
